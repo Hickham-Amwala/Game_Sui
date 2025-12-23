a02aeafef912390e2999371c1432522788e5ccd7
Farizu224 <farizu5436@gmail.com>
2025-12-23 12:00:50 +0700

    Final V 4.0

---
+++ sui/scripts/spike_trap.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+# Tarik node AudioStreamPlayer2D ke sini (pastikan namanya sesuai)
+@onready var trap_sound = $AudioStreamPlayer2D
+
+# Setting berapa detik diamnya (Ganti angka ini sesuai keinginan)
+var waktu_jeda = 0.73 
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	
+	# Mulai mainkan suara saat game mulai
+	putar_suara_loop()
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		print("Player kena duri!")
+		if body.has_method("die"):
+			body.die()
+
+# --- FUNGSI BARU UNTUK LOOPING DENGAN JEDA ---
+func putar_suara_loop():
+	# Cek keamanan: Jika trap sudah dihapus dari game, berhenti looping
+	if not is_inside_tree(): return
+	
+	if trap_sound:
+		# 1. Mainkan Suara
+		trap_sound.play()
+		
+		# 2. Tunggu sampai durasi suara selesai (misal suaranya 0.5 detik)
+		await trap_sound.finished
+		
+		# 3. Tunggu lagi selama waktu jeda (misal 2 detik)
+		await get_tree().create_timer(waktu_jeda).timeout
+		
+		# 4. Panggil diri sendiri lagi (Ulangi dari langkah 1)
+		putar_suara_loop()
